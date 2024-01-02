541c5ed3eb6d79e95fe8b8bba52032fe04ab3924
rikosgrt <122290423+rikosgrt@users.noreply.github.com>
2024-01-02 22:44:27 +0700

    Create Shape.java

---
+++ Shape.java
@@ -0,0 +1,16 @@
+package UAS2;
+
+class Shape {
+    private final String color;
+    public Shape(String color) {
+        this.color = color;
+    }
+    
+    public String getColor() {
+        return color;
+    }
+    
+    public double getArea() {
+        return 0;
+    }
+}
