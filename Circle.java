dc2fdbd5159f26d304a30927d32b6b6c626e653d
rikosgrt <122290423+rikosgrt@users.noreply.github.com>
2024-01-02 22:45:49 +0700

    Create Circle.java

---
+++ Circle.java
@@ -0,0 +1,15 @@
+
+package UAS2;
+
+class Circle extends Shape {
+    private final double radius;
+    public Circle(String color, double radius) {
+        super(color);
+        this.radius = radius;
+    }
+    
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+}
